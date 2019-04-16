d4501146a125567d14bad58bc034d8a6752d1f31
Aaron Austin <aaron@aaronaustin.com>
2019-04-16 10:46:25 -0400

    add child sibling route

--- child-and-sibling-pages-route.php
+++ child-and-sibling-pages-route.php
@@ -7,7 +7,7 @@ add_action( 'rest_api_init', 'child_pages_api_posts_meta_field' );
 
 function child_pages_api_posts_meta_field() {
     // register_rest_field ( 'name-of-post-type', 'name-of-field-to-return', array-of-callbacks-and-schema() );
-    register_rest_field( 'post', 'related_pages', array(
+    register_rest_field( 'page', 'related_pages', array(
            'get_callback'    => 'get_child_and_sibling_pages',
            'schema'          => null,
         )
